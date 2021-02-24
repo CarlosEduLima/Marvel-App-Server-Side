b07015dd5108cfa99f352e1d3703d260ceddd971
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-24 20:51:02 -0300

    validade login httpResquest and httpRequest body

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,5 +1,10 @@
 class LoginRouter {
   route (httpResquest) {
+    if (!httpResquest || !httpResquest.body) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpResquest.body
     if (!email || !password) {
       return {
@@ -31,5 +36,20 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpResquest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('should return 500 if no body is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
