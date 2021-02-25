ab35cebc59a2e93cd0d2a7140776c34ecc18227e
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-25 13:16:56 -0300

    create makesut factore in EmailValidator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -5,15 +5,18 @@ class EmailValidator {
     return validator.isEmail(email)
   }
 }
+const makeSut = () => {
+  return new EmailValidator()
+}
 describe('Email Validator', () => {
   test('Shout return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
   test('Shout return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
