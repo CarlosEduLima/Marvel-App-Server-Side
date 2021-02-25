2f109e795602ae15103a5b1168e2fb1f7440da3d
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-25 09:14:40 -0300

    move helps folder to presentation

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,8 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('./helpers/missing-param-error')
-const UnauthorizedError = require('./helpers/unauthorized-error')
-const ServerError = require('./helpers/server-error')
-const InvalidParamError = require('./helpers/invalid-param-error')
+const MissingParamError = require('../helpers/missing-param-error')
+const UnauthorizedError = require('../helpers/unauthorized-error')
+const ServerError = require('../helpers/server-error')
+const InvalidParamError = require('../helpers/invalid-param-error')
 
 // factore design pattern
 const makeSut = () => {
