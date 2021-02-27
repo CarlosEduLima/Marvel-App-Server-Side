398b75b90900ea7be5513eb7cfddbbe4512999ad
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-27 20:43:40 -0300

    Change add user router method

--- src/main/routes/user-routes.js
+++ src/main/routes/user-routes.js
@@ -1,5 +1,5 @@
 const adpter = require('../config/express-router-adpter')
 const addUserRoute = require('../../presentation/routers/add-user-router')
 module.exports = router => {
-  router.get('/sign-up', adpter(addUserRoute()))
+  router.post('/sign-up', adpter(addUserRoute()))
 }
