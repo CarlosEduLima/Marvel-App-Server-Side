218a4f00de251b2268cea3b93ffee5d1474b5e27
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-27 13:53:17 -0300

    add body-parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const Routes = require('./main/config/routes')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 Routes(app)
-app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 const PORT = process.env.PORT || 3001
