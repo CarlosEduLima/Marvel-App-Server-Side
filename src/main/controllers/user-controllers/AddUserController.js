6cb1d1f1246a0e3720856c17d1c69d5e09c98250
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-27 23:31:41 -0300

    Encrypt user password

--- src/main/controllers/user-controllers/AddUserController.js
+++ src/main/controllers/user-controllers/AddUserController.js
@@ -1,6 +1,7 @@
 const { AddUserCase } = require('../../../domain/user-cases/AddUserCase')
 const HttpResponse = require('../../../presentation/helpers/http-response')
 const UserDb = require('../../../infra/users')
+const { createHash } = require('../../../utils/encrypter')
 module.exports = {
   async AddUserController (httpRequest) {
     const validation = await AddUserCase(httpRequest)
@@ -9,12 +10,18 @@ module.exports = {
       return HttpResponse
     }
 
+    const passwordHash = createHash(httpRequest.body.password)
+    if (!passwordHash) {
+      return HttpResponse.serverError()
+    }
+    httpRequest.body.password = passwordHash
+
     const databaseResponse = await UserDb.insertNewUser(httpRequest.body)
 
     if (!databaseResponse.success) {
       return HttpResponse.serverError()
     }
 
-    return HttpResponse.ok()
+    return HttpResponse.ok('Usuário criado com sucesso')
   }
 }
