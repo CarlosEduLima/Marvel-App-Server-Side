e9758a2b01faa731d70b7f7d9ac866fca3e30da3
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-28 09:48:34 -0300

    add expires param in token-generator

--- src/utils/token-generator.js
+++ src/utils/token-generator.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken')
 const MissingParamError = require('../errors/missing-param-error')
 module.exports = {
-  async  generate (secret, id) {
+  async  generate (secret, id, expires) {
     if (!secret) {
       throw new MissingParamError('secret')
+    }
+    if (!expires) {
+      throw new MissingParamError('expires')
     }
     if (!id) {
       throw new MissingParamError('id')
     }
-    return jwt.sign({ _id: id }, secret)
+    return jwt.sign({ _id: id }, secret, { expiresIn: expires })
   }
 }
